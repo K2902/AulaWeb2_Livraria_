cca4fd7db7632d4f2355aa56db7badddfe821a02
K2902 <bhykw@hotmail.com>
2026-05-07 22:50:51 -0300

    2026.05.07-modificações no dto, validações

--- src/modulos/autores/autores.dto.ts
+++ src/modulos/autores/autores.dto.ts
@@ -2,6 +2,7 @@ import { Expose, Transform } from 'class-transformer';
 import {
   IsEmail,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Matches,
   MaxLength,
@@ -14,8 +15,12 @@ export class CriarAutorDto {
   @IsNotEmpty({ message: 'O nome é obrigatório' })
   @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
   @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
-  @Transform(({ value }) => value.trim()) //remove espaços em branco do nome
-  @Expose({ name: 'nome_autor' }) //expoe o nome para o front-end com o nome nome_autor
+  @Transform(({ value }) => {
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  }) //remove espaços em branco do nome
   @Matches(/^[a-zA-Z\s]+$/, {
     message: 'O nome deve conter apenas letras e espaços',
   }) //expressão regular para validar o nome
@@ -23,8 +28,8 @@ export class CriarAutorDto {
 
   @IsEmail({}, { message: 'O email deve ser um email válido' })
   @IsNotEmpty({ message: 'O email é obrigatório' })
+  @MaxLength(255, { message: 'O email deve ter no máximo 255 caracteres' })
   @Transform(({ value }) => value.trim().toLowerCase()) //remove espaços em branco e converte para minúsculas
-  @Expose({ name: 'email_autor' }) //expoe o email para o front-end com o nome email_autor
   @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
     message: 'O email deve ser um email válido',
   }) //expressão regular para validar o email
@@ -32,7 +37,29 @@ export class CriarAutorDto {
 }
 
 export class AtualizarAutorDto {
-  nome?: string;
-  email?: string;
-  ativo?: boolean;
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsOptional({ message: 'O nome é opcional' })
+  @MinLength(3, { message: 'O nome deve ter pelo menos 3 caracteres' })
+  @MaxLength(100, { message: 'O nome deve ter no máximo 100 caracteres' })
+  @Transform(({ value }) => {
+    const valor = typeof value;
+    if (valor === 'string') {
+      return value.trim();
+    }
+  }) //remove espaços em branco do nome
+  @Matches(/^[a-zA-Z\s]+$/, {
+    message: 'O nome deve conter apenas letras e espaços',
+  }) //expressão regular para validar o nome
+  nome: string;
+
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsOptional({ message: 'O email é opcional' })
+  @MaxLength(255, { message: 'O email deve ter no máximo 255 caracteres' })
+  @Transform(({ value }) => value.trim().toLowerCase()) //remove espaços em branco e converte para minúsculas
+  @Matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+    message: 'O email deve ser um email válido',
+  }) //expressão regular para validar o email
+  email: string;
+
+  // ativo?: boolean;
 }
