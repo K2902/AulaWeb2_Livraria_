49bf5c40bf9f713667a36789dc1c398aa9f6329e
K2902 <bhykw@hotmail.com>
2026-05-21 22:33:35 -0300

    2026.05.21-FixCriarLivro+ModuleImportsExports

--- src/modulos/livros/livros.service.ts
+++ src/modulos/livros/livros.service.ts
@@ -5,17 +5,22 @@ import {
 } from '@nestjs/common';
 import { LivrosRepository } from './livros.repository';
 import { CriarLivroDto } from './livros.dto';
+import { AutoresService } from '../autores/autores.service';
 
 @Injectable()
 export class LivrosService {
-  constructor(private readonly livrosRepository: LivrosRepository) {}
+  constructor(
+    private readonly livrosRepository: LivrosRepository,
+    private readonly AutoresService: AutoresService,
+  ) {}
 
   async listarLivros() {
     return await this.livrosRepository.listarLivros();
   }
 
   async criarLivro(bodyRequest: CriarLivroDto) {
-    return await this.livrosRepository.criarLivro(bodyRequest),
+    await this.AutoresService.listarAutor(bodyRequest.id_autor);
+    return await this.livrosRepository.criarLivro(bodyRequest);
   }
 
   // async listarLivro(id: number) {
