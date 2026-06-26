cb3c74d1732b5dffed3101a5d7bbcbc6bdf1992f
Bruno H <bhykw@hotmail.com>
2026-06-25 22:43:56 -0300

    2026.06.25-Adiciona método POST para criar usuário no controller de usuários, integrando o DTO CriarUsuarioDto para validação de dados.

--- src/modulos/usuarios/usuarios.controller.ts
+++ src/modulos/usuarios/usuarios.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
+import { CriarUsuarioDto } from './usuarios.dto';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
+  
+  @Post('criar-usuario')
+  async criarUsuario(@Body() bodyRequest: CriarUsuarioDto){
+    return await this.usuariosService.criarUsuario(bodyRequest)
+  }
 
 }  
